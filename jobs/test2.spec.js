0189a081ee929c5f763cfd33fcd727a2619d4249
Shubham Thakur <shuthakur@adobe.com>
2025-09-08 22:17:33 +0530

    test

--- jobs/test2.spec.js
+++ jobs/test2.spec.js
@@ -1,16 +1,9 @@
-// jobs/test.spec.js
 import { test, expect } from '@playwright/test';
 
 test('Wikipedia search works', async ({ page }) => {
-  // Wikipedia open karo
   await page.goto('https://www.wikipedia.org/');
-
-  // Search input me text likho
   await page.fill('input[name="search"]', 'Selenium (software)');
-
-  // Search button pe click karo
   await page.click('i[data-jsl10n="search-input-button"]');
 
-  // Title verify karo
   await expect(page).toHaveTitle('Selenium (software) - Wikipedia');
 });
