d65f0cf95aaf8c51ebe87b1f94a2ec2c13e1090b
Shubham Thakur <shuthakur@adobe.com>
2025-09-08 22:08:56 +0530

    test

--- jobs/test2.spec.js
+++ jobs/test2.spec.js
@@ -1,29 +1,16 @@
-const { chromium } = require('playwright/test');
-const assert = require('assert');
+// jobs/test.spec.js
+import { test, expect } from '@playwright/test';
 
-(async () => {
-  const browser = await chromium.launch({ headless: false }); // headless: false se browser window dikhegi
-  const page = await browser.newPage();
+test('Wikipedia search works', async ({ page }) => {
+  // Wikipedia open karo
   await page.goto('https://www.wikipedia.org/');
 
-  // Search input field me text likho
+  // Search input me text likho
   await page.fill('input[name="search"]', 'Selenium (software)');
 
   // Search button pe click karo
   await page.click('i[data-jsl10n="search-input-button"]');
 
-  // Page load hone tak rukho (title me "Selenium" aane tak)
-  await page.waitForLoadState('domcontentloaded');
-  const title = await page.title();
-  console.log('Page title:', title);
-
-  assert.strictEqual(
-    title,
-    'Selenium (software) - Wikipedia',
-    'Title did not match'
-  );
-
-  console.log('✅ Title matched successfully');
-
-  await browser.close();
-})();
+  // Title verify karo
+  await expect(page).toHaveTitle('Selenium (software) - Wikipedia');
+});
